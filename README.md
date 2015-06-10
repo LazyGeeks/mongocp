458ae44700f5ad490e49ef595b1d3612e638dfa4
RussellLuo <luopeng.he@gmail.com>
2015-06-10 12:13:57 +0800

    The first version of mongocp

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # mongocp
+
 A utility helps you copy data from remote MongoDB database to local one.
+
+
+## Usage
+
+### 1. Set the operation host
+
+```
+$ vi fabfile.py
+@hosts('user@operation-host')
+```
+
+### 2. Start to copy
+
+```
+$ fab cp:<remote-mongodb-host>,<from-db>,<from-collection>,<query>,<local-mongodb-host>
+```
+
+
+## Todo
+
++ Make the operation host customizable
++ Wrap this fabfile as an user-friendly command
++ Support more `mongodump` and `mongorestore` arguments and options
---
+++ fabfile.py
@@ -0,0 +1,41 @@
+import os
+import tempfile
+
+from fabric.api import task, hosts, local, run, get
+
+
+@task
+@hosts('user@operation-host')
+def cp(from_host, from_db, from_collection, query,
+       to_host, to_db=None, to_collection=None):
+    """Copy data from remote MongoDB database to local one."""
+    to_db = to_db or from_db
+    to_collection = to_collection or from_collection
+
+    # Dump remotely
+    dump_tmp = tempfile.mkdtemp()
+    run(
+        'mongodump --authenticationDatabase admin -uroot -proot '
+        '-h %s -d %s -c %s -q %r -o %s' % (
+            from_host, from_db, from_collection, query, dump_tmp
+        )
+    )
+
+    # Download
+    restore_tmp = dump_tmp
+    get(dump_tmp, os.path.dirname(restore_tmp))
+
+    # Clean up remote temporary directory
+    run('rm -rf %s' % dump_tmp)
+
+    # Restore locally
+    local(
+        'mongorestore --authenticationDatabase admin -uroot -proot '
+        '-h %s -d %s -c %s %s/%s/%s.bson' % (
+             to_host, to_db, to_collection,
+             restore_tmp, from_db, from_collection
+        )
+    )
+
+    # Clean up local temporary directory
+    local('rm -rf %s' % restore_tmp)
